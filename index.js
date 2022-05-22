81d79419c3c81b26c49dc923c763ab14fac4b775
Sam-Dunlap <sam.dunlap15@gmail.com>
2022-05-21 20:15:38 -0700

    First Commit

---
+++ index.js
@@ -0,0 +1,44 @@
+function App(){
+  const [todos, setTodos] = React.useState([
+    {
+      text: 'learn react',
+      isCompleted: false,
+    },
+    {
+      text: 'meet friend for lunch',
+      isCompleted: false,
+    },
+    {
+      text: 'build todo app',
+      isCompleted: false,
+    }        
+  ])
+
+  const addTodo = text => {
+    const newTodo = [...todos, {text:text, isCompleted:false}];
+    setTodos(newTodo);
+  }
+
+  const removeTodo = e => {
+    const index = Number(e.target.id);
+    let temp = [...todos];
+    temp.splice(index, 1);
+    setTodos(temp);
+  }
+  return(
+    <div className="app">
+
+      <div className="todo-list">
+        {todos.map((todo, i) => (
+          <div style={{background: color(i)}} className="todo" key={i} id={i} onClick={removeTodo}>{todo.text}</div>
+          ))}
+        <TodoForm addTodo={addTodo}/>
+      </div>
+    </div>
+  );
+}
+
+ReactDOM.render(
+  <App/>,
+  document.getElementById('root')
+);
---
+++ todocolors.js
@@ -0,0 +1,4 @@
+function color(n) {
+  if (n % 2 == 0) return '#9eeef0'
+  return '#6fe065'
+}
